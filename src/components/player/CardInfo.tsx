6b60f4aa02c4d9b86d78998362849140dcea2c8f
codeals <alejo901003@hotmail.com>
2023-08-03 19:17:39 +0200

    unused dep

--- src/components/player/CardInfo.tsx
+++ src/components/player/CardInfo.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 
 import { Episode } from '../../models/Episode';
