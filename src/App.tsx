9b393154f0fd3e9902fe788b449cdfded0088046
codeals <alejo901003@hotmail.com>
2023-08-04 07:31:16 +0200

    adding quicksand font

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ErrorBoundary } from 'react-error-boundary';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import 'typeface-quicksand';
 
 import './App.css';
 import { publicRoutes } from "./routes/app/routes";
@@ -8,32 +10,42 @@ import Layout from './components/layout/Layout';
 import FallbackError from './components/layout/FallbackError';
 import EpisodesContextProvider from './contexts/episodesContext';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Quicksand",
+    ].join(",")
+  }
+});
+
 const App: React.FC = () => {
   return (
     <ErrorBoundary
       FallbackComponent={FallbackError}
     >
-      <React.Fragment>
-        <EpisodesContextProvider>
-          <Suspense fallback={true}>
-            <Router>
-              <Routes>
-                {publicRoutes.map((route) => (
-                  <Route 
-                    key={route.id} 
-                    path={route.path} 
-                    element={
-                      <Layout>
-                        {route.component}
-                      </Layout>
-                    } 
-                  />
-                ))}
-              </Routes>
-            </Router>
-          </Suspense>
-        </EpisodesContextProvider>
-      </React.Fragment>
+      <ThemeProvider theme={theme}>
+        <React.Fragment>
+          <EpisodesContextProvider>
+            <Suspense fallback={true}>
+              <Router>
+                <Routes>
+                  {publicRoutes.map((route) => (
+                    <Route 
+                      key={route.id} 
+                      path={route.path} 
+                      element={
+                        <Layout>
+                          {route.component}
+                        </Layout>
+                      } 
+                    />
+                  ))}
+                </Routes>
+              </Router>
+            </Suspense>
+          </EpisodesContextProvider>
+        </React.Fragment>
+      </ThemeProvider>
     </ErrorBoundary>
   );
 };
